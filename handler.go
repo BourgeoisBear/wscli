ea1779dffcd38215556cae90cd79930fe519def4
Jason Stewart <support@eggplantsd.com>
2022-10-27 18:41:01 -0400

    repo create
    Signed-off-by: Jason Stewart <support@eggplantsd.com>

---
+++ handler.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"sync"
+	"time"
+
+	gws "github.com/gorilla/websocket"
+)
+
+type HandlerErr int
+
+const (
+	ErrNilWs HandlerErr = iota
+)
+
+const (
+	TextMessage   = gws.TextMessage
+	BinaryMessage = gws.BinaryMessage
+	CloseMessage  = gws.CloseMessage
+	PingMessage   = gws.PingMessage
+	PongMessage   = gws.PongMessage
+)
+
+func (err HandlerErr) Error() string {
+	switch err {
+	case ErrNilWs:
+		return "nil websocket connection"
+	}
+	return "unknown HandlerErr"
+}
+
+type Handler struct {
+	wsConn              *gws.Conn
+	dlRead, dlWrite     time.Duration
+	chRdrErr, chPingErr chan error
+
+	// signal completion from goroutines
+	chRdrDone, chPingDone chan struct{}
+
+	// signal completion from caller
+	chCloseAll chan struct{}
+
+	mtxWri sync.Mutex // setup + write mutex
+}
+
+type MsgReaderFunc func(pHdl *Handler, nType int, iRdr io.Reader, err error) bool
+
+func Dial(url string, hdr http.Header) (*gws.Conn, *http.Response, error) {
+	return gws.DefaultDialer.Dial(url, hdr)
+}
+
+func (ss *Handler) GetConn() *gws.Conn {
+	return ss.wsConn
+}
+
+func (ss *Handler) Wait() (errRdr, errPing error) {
+	// wait for readpump completion, capture exit err
+	if ss.chRdrDone != nil {
+		<-ss.chRdrDone
+		errRdr = <-ss.chRdrErr
+		ss.chRdrDone = nil
+		ss.chRdrErr = nil
+	}
+	// wait for pingpump completion, capture exit err
+	if ss.chPingDone != nil {
+		<-ss.chPingDone
+		errPing = <-ss.chPingErr
+		ss.chPingDone = nil
+		ss.chPingErr = nil
+	}
+	// clean up
+	ss.chCloseAll = nil
+	ss.wsConn = nil
+	return
+}
+
+func (ss *Handler) Close() error {
+	if ss.chCloseAll != nil {
+		close(ss.chCloseAll)
+	}
+	if ss.wsConn == nil {
+		return ErrNilWs
+	} else {
+		// in case deadlines aren't in use
+		return ss.wsConn.Close()
+	}
+}
+
+func (ss *Handler) WriteMessage(nType int, bsMsg []byte) error {
+
+	if ss.wsConn == nil {
+		return ErrNilWs
+	}
+
+	ss.mtxWri.Lock()
+	defer ss.mtxWri.Unlock()
+
+	if ss.dlWrite > 0 {
+		if err := ss.wsConn.SetWriteDeadline(time.Now().Add(ss.dlWrite)); err != nil {
+			return err
+		}
+	}
+	return ss.wsConn.WriteMessage(nType, bsMsg)
+}
+
+func StartHandler(
+	pwsConn *gws.Conn,
+	pingInterval, readTimeout, writeTimeout time.Duration,
+	fnHandleMsg MsgReaderFunc,
+) (*Handler, error) {
+
+	if pwsConn == nil {
+		return nil, ErrNilWs
+	}
+
+	// housekeeping
+	ss := new(Handler)
+	ss.wsConn = pwsConn
+
+	ss.dlRead = readTimeout
+	ss.dlWrite = writeTimeout
+
+	ss.chCloseAll = make(chan struct{})
+	ss.chRdrDone = make(chan struct{})
+	ss.chRdrErr = make(chan error, 1)
+
+	// read pump
+	go func() {
+
+		var err error
+		defer func() {
+			close(ss.chRdrDone)
+			ss.chRdrErr <- err
+		}()
+
+		for {
+			select {
+			case <-ss.chCloseAll:
+				return
+			case <-ss.chPingDone:
+				// stop on ping pump termination
+				return
+			default:
+				if ss.dlRead > 0 {
+					if err = ss.wsConn.SetReadDeadline(time.Now().Add(ss.dlRead)); err != nil {
+						return
+					}
+				}
+				var mtype int
+				var iRdr io.Reader
+				mtype, iRdr, err = ss.wsConn.NextReader()
+				if !fnHandleMsg(ss, mtype, iRdr, err) {
+					return
+				}
+			}
+		}
+	}()
+
+	// ping ticker
+	if pingInterval > 0 {
+
+		ss.chPingDone = make(chan struct{})
+		ss.chPingErr = make(chan error, 1)
+
+		go func() {
+
+			ticker := time.NewTicker(pingInterval)
+
+			var err error
+			defer func() {
+				ticker.Stop()
+				close(ss.chPingDone)
+				ss.chPingErr <- err
+			}()
+
+			for {
+				select {
+				case <-ss.chCloseAll:
+					return
+				case <-ss.chRdrDone:
+					// stop on read pump termination
+					return
+				case _ = <-ticker.C:
+					if err = ss.WriteMessage(gws.PingMessage, nil); err != nil {
+						return
+					}
+				}
+			}
+		}()
+	}
+
+	// mutexed writer for ping handler + deadline updating for recvd ping msg
+	pwsConn.SetPingHandler(func(szMsg string) error {
+		if err := ss.WriteMessage(gws.PongMessage, nil); err != nil {
+			return err
+		}
+		if readTimeout > 0 {
+			return pwsConn.SetReadDeadline(time.Now().Add(readTimeout))
+		}
+		return nil
+	})
+
+	return ss, nil
+}
---
+++ wscli.go
@@ -0,0 +1,299 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"regexp"
+	"runtime/debug"
+	"strings"
+	"time"
+)
+
+func fnErrAbort(prfx string, err error) {
+	errfunc(prfx, err, true)
+}
+
+func fnErr(prfx string, err error) {
+	errfunc(prfx, err, false)
+}
+
+func errfunc(prfx string, err error, bAbort bool) {
+	const SA = "\x1b[91m"
+	const SZ = "\x1b[0m"
+
+	if err != nil {
+		if len(prfx) > 0 {
+			fmt.Fprintln(os.Stderr, SA+strings.ToUpper(prfx)+": "+err.Error()+SZ)
+		} else {
+			fmt.Fprintln(os.Stderr, SA+err.Error()+SZ)
+		}
+		if bAbort {
+			os.Exit(-1)
+		}
+	}
+}
+
+type fsm struct {
+	pH       *Handler
+	hdr      http.Header
+	msg      []byte
+	msgEnd   []byte
+	rxMsgEnd *regexp.Regexp
+}
+
+func newFsm() *fsm {
+	return &fsm{
+		hdr:      make(http.Header),
+		msg:      make([]byte, 0, 4096),
+		rxMsgEnd: regexp.MustCompile(`\\msg\s+(.*)`),
+	}
+}
+
+func (pf *fsm) closeWait() {
+	if pf.pH == nil {
+		return
+	}
+	err := pf.pH.Close()
+	fnErr("ws close", err)
+
+	pf.pH.Wait()
+	pf.pH = nil
+
+	pf.hdr = make(http.Header)
+	pf.resetMsg()
+}
+
+func (pf *fsm) resetMsg() {
+	pf.msg = pf.msg[:0]
+	pf.msgEnd = nil
+}
+
+func (pf *fsm) processLine(line []byte) {
+
+	// panic recover
+	defer func() {
+		if r := recover(); r != nil {
+			stk := debug.Stack()
+			fmt.Fprintf(os.Stderr, "\x1b[91;1mPANIC: %v\x1b[0m\n", r)
+			lines := bytes.Split(stk, []byte{'\n'})
+			for _, ln := range lines {
+				fmt.Fprint(os.Stderr, "\t", string(ln), "\n")
+			}
+		}
+	}()
+
+	if pf.msgEnd != nil {
+
+		// fmt.Printf("TERMINATOR %#v\n", string(pf.msgEnd))
+
+		if bytes.Equal(line, pf.msgEnd) {
+
+			// fmt.Printf("SEND %#v\n", string(pf.msg))
+			err := pf.pH.WriteMessage(TextMessage, pf.msg)
+			pf.resetMsg()
+			if err != nil {
+				fnErr("WS WRITE", err)
+				return
+			}
+			fmt.Print("\x1b[96m", "SENT", "\x1b[0m\n")
+
+		} else {
+
+			// fmt.Printf("APPEND %#v\n", string(line))
+			pf.msg = append(pf.msg, line...)
+		}
+
+		return
+	}
+
+	switch {
+
+	// begin message heredoc
+	case bytes.HasPrefix(line, []byte("\\msg")):
+		pf.resetMsg()
+		sMtch := pf.rxMsgEnd.FindSubmatch(line)
+		if len(sMtch) < 2 {
+			pf.msgEnd = nil
+		} else {
+			pf.msgEnd = make([]byte, len(sMtch[1]))
+			copy(pf.msgEnd, sMtch[1])
+		}
+		pf.msgEnd = append(pf.msgEnd, '\n')
+
+	// clear headers
+	case bytes.HasPrefix(line, []byte("\\hdrclr")):
+		pf.hdr = make(http.Header)
+
+	// list headers
+	case bytes.HasPrefix(line, []byte("\\hdrlst")):
+		for k := range pf.hdr {
+			fmt.Print(
+				"\t\x1b[93m",
+				k,
+				":\x1b[0m ",
+				fmt.Sprintf("%v", pf.hdr[k]),
+				"\n",
+			)
+		}
+
+	// hang-up
+	case bytes.HasPrefix(line, []byte("\\hup")):
+		pf.closeWait()
+
+	// dial
+	case bytes.HasPrefix(line, []byte("\\dial ws")):
+
+		line = bytes.TrimPrefix(line, []byte("\\dial "))
+		if line = bytes.TrimSpace(line); len(line) == 0 {
+			return
+		}
+		pf.closeWait()
+		pC, _, err := Dial(string(line), pf.hdr)
+		if err != nil {
+			fnErr("WS DIAL", err)
+			return
+		}
+		pf.pH, err = StartHandler(pC, 10*time.Second, 0, 0, fnHandleMsg)
+		if err != nil {
+			fnErr("WS HANDLER", err)
+			return
+		}
+
+	// add/remove HTTP headers
+	case len(line) > 0:
+
+		// parse & add header
+		parts := bytes.SplitN(line, []byte(":"), 2)
+		if len(parts) < 2 {
+			return
+		}
+
+		// get key
+		hk := string(bytes.TrimSpace(parts[0]))
+		if len(hk) == 0 {
+			return
+		}
+
+		// get value
+		// del from map on empty, otherwise add
+		hv := string(bytes.TrimSpace(parts[1]))
+		if len(hv) == 0 {
+			pf.hdr.Del(hk)
+		} else {
+			pf.hdr.Add(hk, hv)
+		}
+	}
+}
+
+// websocket -> stdout
+func fnHandleMsg(pHdl *Handler, nType int, iRdr io.Reader, err error) bool {
+
+	// exit on websocket error
+	if err != nil {
+		return false
+	}
+
+	// read full response
+	bsMsg, err := io.ReadAll(iRdr)
+	if err != nil {
+		fnErr("websocket->stdout", err)
+		return false
+	}
+
+	// write to stdout
+	if len(bsMsg) > 0 {
+
+		// header
+		szFrom := ""
+		if pC := pHdl.GetConn(); pC != nil {
+			szFrom = pC.RemoteAddr().String()
+		}
+		now := time.Now().Round(0)
+		fmt.Print(
+			"\x1b[37m",
+			now.Format(time.RFC3339),
+			", RCV FROM ",
+			szFrom,
+			":\x1b[0m\n",
+		)
+
+		// message, line-by-line
+		lines := bytes.Split(bsMsg, []byte{'\n'})
+		for _, ln := range lines {
+			fmt.Println("\t\x1b[92m" + string(ln) + "\x1b[0m")
+		}
+	}
+
+	return true
+}
+
+func main() {
+
+	const helpPrefix = `wscli
+	command-line interface to a websocket connection
+
+COMMANDS
+
+	Connect to Websocket
+		\dial ws://127.0.0.1:8080/ws
+
+	Disconnect from Websocket
+		\hup
+
+	Send Message (heredoc format)
+		\msg [message terminator]
+		message line 1
+		message line 2
+		message line 3
+		[message terminator]
+
+		(default terminator is a blank line if left unspecified)
+
+	Specify HTTP Headers
+		Authorization: awo875pu84uj6paj436up
+		Content-Type: application/json
+
+	List Specified HTTP Headers
+		\hdrlst
+
+	Clear Specific HTTP Header (key without value)
+		Authorization:
+
+	Clear All Specified HTTP Headers
+		\hdrclr`
+
+	// HELP MESSAGE
+	var iWri io.Writer = os.Stdout
+	flag.CommandLine.SetOutput(iWri)
+	flag.Usage = func() {
+		fmt.Fprint(iWri, helpPrefix)
+		flag.PrintDefaults()
+		fmt.Fprint(iWri, "\n")
+	}
+	flag.Parse()
+
+	buf := make([]byte, 4096)
+	fsm := newFsm()
+
+	for {
+
+		n, rdErr := os.Stdin.Read(buf)
+		if rdErr != nil && rdErr != io.EOF {
+			fnErrAbort("stdin read", rdErr)
+			break
+		}
+
+		if n > 0 {
+			fsm.processLine(buf[0:n])
+		}
+
+		// fifo without a writer does not block, so throttle just in case
+		if rdErr == io.EOF {
+			time.Sleep(time.Second / 2)
+		}
+	}
+}
